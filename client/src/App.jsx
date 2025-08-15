4d8e3518e659d8fb2e2bb5b647e6196a63eb9f3f
alokrathod <rathodalok94@gmail.com>
2025-08-15 11:19:56 +0530

    auth frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,24 @@ import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
 import { AppContext } from "./context/AppContext";
+import MyOrders from "./pages/MyOrders";
+import Auth from "./pages/Auth";
 
 const App = () => {
-  const { isSeller } = useContext(AppContext);
+  const { isSeller, showUserLogin } = useContext(AppContext);
   const isSellerPath = useLocation().pathname.includes("seller");
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Auth /> : null}
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/my-orders" element={<MyOrders />} />
         </Routes>
       </div>
     </div>
