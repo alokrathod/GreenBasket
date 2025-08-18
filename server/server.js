7b540619f4b117be2330ae6e716e6e1c203b2582
alokrathod <rathodalok94@gmail.com>
2025-08-18 13:48:22 +0530

    started backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-// hi
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const allowedOrigins = [process.env.CLIENT_URL];
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(express.json());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cookieParser());
+
+// API endpoints
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+// start the server
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:", PORT);
+});
